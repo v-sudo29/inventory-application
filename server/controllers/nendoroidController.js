c17c1094ce352ff3f8b29e9d1260bbf4fc4e71a4
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-08-02 15:37:57 -0700

    Modify nendoroid model and controller

--- server/controllers/nendoroidController.js
+++ server/controllers/nendoroidController.js
@@ -2,7 +2,7 @@ const Nendoroid = require('../models/nendoroid')
 
 const asyncHandler = require("express-async-handler");
 
-// Display list of all Nendoroid
+// GET list of all Nendoroid home page
 exports.nendoroid_list = asyncHandler(async (req, res, next) => {
   const allNendoroids = await Nendoroid.find().exec()
   res.json(allNendoroids)
--- server/models/nendoroid.js
+++ server/models/nendoroid.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const NendoroidSchema = new mongoose.Schema({
   name: { type: String, required: true, minLength: 1 },
   price: { type: String, required: true, default: 'N/A'},
-  description: { type: String, required: true, default: 'Description is not available at this time.' }
+  description: { type: String, required: true, default: 'Description is not available at this time.' },
+  imageUrl: { type: String, required: true }
 })
 
 module.exports = mongoose.model('Nendoroid', NendoroidSchema)
