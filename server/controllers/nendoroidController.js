8ba42efaf7e5004e529d9d94b8b9f31b6eff1d85
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-08-03 09:36:47 -0700

    Modify controller to remove redirect

--- server/controllers/nendoroidController.js
+++ server/controllers/nendoroidController.js
@@ -2,8 +2,15 @@ const Nendoroid = require('../models/nendoroid')
 
 const asyncHandler = require("express-async-handler");
 
-// GET list of all Nendoroid home page
+// GET list of all Nendoroids
 exports.nendoroid_list = asyncHandler(async (req, res, next) => {
   const allNendoroids = await Nendoroid.find().exec()
   res.json(allNendoroids)
+})
+
+// POST new Nendoroid
+exports.nendoroid_create_post = asyncHandler(async (req, res, next) => {
+  Nendoroid.create(req.body)
+    .then(data => res.json(data))
+    .catch(err => res.json(err))
 })
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,4 +5,7 @@ const nendoroid_controller = require('../controllers/nendoroidController')
 // GET home page, inventory list
 router.get('/', nendoroid_controller.nendoroid_list)
 
+// POST request for creating Nendoroid
+router.post('/create', nendoroid_controller.nendoroid_create_post)
+
 module.exports = router
