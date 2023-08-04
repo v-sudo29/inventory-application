27b8c7451ce10cd80350980540cc368d0c4eb445
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-08-04 11:26:29 -0700

    Implement POST route to update Nendoroid Automate file deletion if new file uploaded

--- server/controllers/nendoroidController.js
+++ server/controllers/nendoroidController.js
@@ -1,5 +1,8 @@
 const Nendoroid = require('../models/nendoroid')
-const asyncHandler = require("express-async-handler");
+const asyncHandler = require('express-async-handler');
+const upload = require('../helpers/upload')
+const path = require('path')
+const fs = require('fs')
 
 // GET list of all Nendoroids
 exports.nendoroid_list = asyncHandler(async (req, res, next) => {
@@ -28,4 +31,44 @@ exports.nendoroid_create_post = asyncHandler(async (req, res, next) => {
       console.log('Nendoroid saved to database')
     })
     .catch(err => res.json(err))
+})
+
+// POST request to update Nendoroid
+exports.nendoroid_update_post = asyncHandler(async (req, res, next) => {
+  const nendoroid = await Nendoroid.findById(req.params.id)
+  let updatedNendoroid
+
+  // Check if new file uploaded 
+  if (!req.file) {
+    updatedNendoroid = new Nendoroid ({
+      name: req.body.name,
+      price: req.body.price,
+      description: req.body.description,
+      units: req.body.units,
+      imageUrl: nendoroid.imageUrl,
+      _id: req.params.id // This is required,  or a new ID will be assigned
+    })
+  } else {
+    // Add file to public dir (multer)
+    upload.single('file')
+
+    // Delete old image file from public dir
+    const oldImgPath = path.join(__dirname, '../public', 'images', `${nendoroid.imageUrl}`)
+    try {
+      await fs.promises.unlink(oldImgPath)
+    } catch (error) {
+      console.log(error)
+    }
+
+    updatedNendoroid = new Nendoroid ({
+      name: req.body.name,
+      price: req.body.price,
+      description: req.body.description,
+      units: req.body.units,
+      imageUrl: req.file.filename,
+      _id: req.params.id // This is required,  or a new ID will be assigned
+    })
+  }
+  const result = await Nendoroid.findByIdAndUpdate(req.params.id, updatedNendoroid, {})
+  res.json(result)
 })
--- server/routes/index.js
+++ server/routes/index.js
@@ -9,7 +9,10 @@ router.get('/', nendoroid_controller.nendoroid_list)
 // GET nendoroid detail
 router.get('/nendoroid/:id', nendoroid_controller.nendoroid_detail)
 
+// POST request for updating Nendoroid
+router.post('/nendoroid/:id/update', upload.single('file'), nendoroid_controller.nendoroid_update_post)
+
 // POST request for creating Nendoroid
-router.post('/create', upload.single('file'), nendoroid_controller.nendoroid_create_post)
+router.post('/create', nendoroid_controller.nendoroid_create_post)
 
 module.exports = router
