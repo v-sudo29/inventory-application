16a279c33664a8156bee408a2e147ee804e60e89
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2023-08-07 15:15:04 -0700

    Extend sidebar to full view height

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -5,13 +5,16 @@ export default function Sidebar() {
   const navStyles = {
     'display': 'flex',
     'flexDirection': 'column',
-    'padding': '1rem',
+    'padding': '3rem',
     'height': '100%',
-    'gap': '1.5rem'
+    'gap': '1.5rem',
+    'fontSize': '1.5rem'
   }
 
   return (
     <Stack
+      pos='sticky'
+      top='0'
       flex='column'
       minW='18rem'
       color='cyan.500'
@@ -19,8 +22,10 @@ export default function Sidebar() {
       boxShadow='base'
       p='10rem 1rem'
     >
-      <nav className="nav-links" style={navStyles as React.CSSProperties}>
+      <nav style={navStyles as React.CSSProperties}>
+        <NavLink to='/'>Home</NavLink>
         <NavLink to='/'>Catalog</NavLink>
+        <NavLink to='/'>Orders</NavLink>
       </nav>
     </Stack>
   )
